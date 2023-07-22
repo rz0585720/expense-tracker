1a5e15a3f9388d71b5148a3cfed6fbfe17692c47
rz0585720 <cherish05061231@gmail.com>
2023-07-22 18:48:44 +0800

    add update expense function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const { route } = require('./home')
 
 router.get('/new', (req, res) => {
   res.render('new')
@@ -14,7 +15,25 @@ router.post('/', (req, res) => {
 })
 
 router.get('/:id/edit', (req, res) => {
-  res.render('edit')
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then(record => {
+      record.date = record.date.toISOString().slice(0, 10)
+      res.render('edit', { record })
+    })
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => {
+      record = Object.assign(record, req.body)
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 module.exports = router
