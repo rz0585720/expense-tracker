69f72dd1b226b813613646c2040aaf9b1ba288f1
rz0585720 <cherish05061231@gmail.com>
2023-07-22 23:50:36 +0800

    add filter function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const Handlebars = require('handlebars')
+const hbsHelpers = require('handlebars-helpers')
 
 
 if (process.env.NODE_ENV !== 'production') {
@@ -13,9 +15,10 @@ require('./config/mongoose')
 
 const port = process.env.PORT || 3000
 const app = express()
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: hbsHelpers }))
 app.set('view engine', 'hbs')
 
+Handlebars.registerHelper('dateFormat', require('handlebars-dateformat'))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,19 +4,55 @@ const Record = require('../../models/record')
 const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
-  Record.find()
-    .populate('categoryId')
-    .lean()
-    .sort({ date: 'desc' })
-    .then(records => {
-      let totalAmount = 0
-      records.forEach(record => {
-        totalAmount += record.amount
-        record.date = record.date.toISOString().slice(0, 10)
-      })
-      res.render('index', { records, totalAmount })
-    })
-    .catch(error => console.log(error))
+	Category.find()
+		.lean()
+		.then(categories => {
+			Record.find()
+				.populate('categoryId')
+				.lean()
+				.sort({ date: 'desc' })
+				.then(records => {
+					let totalAmount = 0
+					records.forEach(record => {
+						totalAmount += record.amount
+						record.date = record.date.toISOString().slice(0, 10)
+					})
+					res.render('index', { categories, records, totalAmount })
+				})
+				.catch(error => console.log(error))
+		})
+})
+
+router.get('/search', (req, res) => {
+	const categoryId = req.query.category
+	let totalAmount = ''
+
+	if (!categoryId) {
+		return res.redirect('/')
+	}
+
+	Category.find()
+		.lean()
+		.then(categories => {
+			categories.forEach(category => {
+				if (category._id.toString() === categoryId) {
+					category.selected = true
+				} else {
+					category.selected = false
+				}
+			})
+			Record.find({ categoryId })
+				.populate('categoryId')
+				.lean()
+				.sort({ date: 'desc' })
+				.then(records => {
+					records.forEach(record => {
+						totalAmount += record.amount
+					})
+					res.render('index', { categories, records, totalAmount })
+				})
+				.catch(error => console.log(error))
+		})
 })
 
 module.exports = router
