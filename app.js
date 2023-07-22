e207c38659c0fccda103c5db7efe9dc7dfdeb2cf
rz0585720 <cherish05061231@gmail.com>
2023-07-22 14:49:42 +0800

    add Schema

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const routes = require('./routes')
 
 if (process.env.NODE_ENV !== 'production') {
@@ -16,6 +17,7 @@ app.set('view engine', 'hbs')
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use(routes)
 
