0ccd55afa5020e045dfdcc8f342ef9a29ca17fd4
rz0585720 <cherish05061231@gmail.com>
2023-07-23 18:31:49 +0800

    add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,46 +4,77 @@ const User = require('../../models/user')
 const passport = require('passport')
 
 router.get('/login', (req, res) => {
-  res.render('login')
+	res.render('login')
 })
 
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
+router.post(
+	'/login',
+	(req, res, next) => {
+		const { email, password } = req.body
+		if (!email || !password) {
+			req.flash('warning_msg', '請先登入才能使用！')
+			return res.redirect('/users/login')
+		}
+		next()
+	},
+	passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/users/login',
+	})
+)
 
 router.get('/register', (req, res) => {
-  res.render('register')
+	res.render('register')
 })
 
 router.post('/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ email }).then(user => {
-    if (user) {
-      console.log('User already exists.')
-      res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    } else {
-      // 如果還沒註冊：寫入資料庫
-      return User.create({
-        name,
-        email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-    }
-  })
-  .catch(err => console.log(err))
+	const { name, email, password, confirmPassword } = req.body
+	const errors = []
+
+	if (!email || !password || !confirmPassword) {
+		errors.push({ message: '所有欄位皆為必填。' })
+	}
+
+	if (password !== confirmPassword) {
+		errors.push({ message: '密碼與確認密碼不相符。' })
+	}
+	if (errors.length) {
+		return res.render('register', {
+			errors,
+			name,
+			email,
+			password,
+			confirmPassword,
+		})
+	}
+
+	User.findOne({ email })
+		.then(user => {
+			if (user) {
+				console.log('User already exists.')
+				res.render('register', {
+					name,
+					email,
+					password,
+					confirmPassword,
+				})
+			} else {
+				return User.create({
+					name,
+					email,
+					password,
+				})
+					.then(() => res.redirect('/'))
+					.catch(err => console.log(err))
+			}
+		})
+		.catch(err => console.log(err))
 })
 
 router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/users/login')
+	req.logout()
+	req.flash('success_msg', '你已經成功登出。')
+	res.redirect('/users/login')
 })
 
 module.exports = router
